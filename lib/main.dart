70d114e06ab463ecf0f17d9fca550083893ebb92
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-08-25 18:28:55 -0400

    Refactor intl into presentation layer

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tecnical_test/config/config_exporter.dart';
-import 'package:tecnical_test/l10n/l10n_exporter.dart';
+import 'package:tecnical_test/presentation/l10n/l10n_exporter.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
