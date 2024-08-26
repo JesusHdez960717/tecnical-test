001bc0b51a62f443808d5e7451dfb41bcf8d1798
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-08-25 20:50:59 -0400

    Add gender icons assets

--- lib/config/assets_config.dart
+++ lib/config/assets_config.dart
@@ -2,4 +2,7 @@ class AssetsConfig {
   static const String markerUrl = 'assets/map/marker.svg';
 
   static const String myLocationUrl = 'assets/map/my-location.svg';
+
+  static const String male = 'assets/icons/male.svg';
+  static const String female = 'assets/icons/female.svg';
 }
